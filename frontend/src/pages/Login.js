fea64c2a6eec6f261a9b9f664fdd7856aa7011b7
yuchangseuk <ckdtjr428@naver.com>
2025-03-30 23:24:04 +0900

    feat. 로그인페이지 프로토타입 생성 #11
    - 로고 및 문장 추가
    - 로그인 ui 및 아이디, 비밀번호 작성칸 추가

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import styles from "../Styles/Login.module.css";
+import logoImage from "../assets/Study with us Logo.png";
 
 function Login() {
   return (
     <div className={styles.div}>
-      <h1>hello</h1>
-      <input className={styles.IDinput} placeholder="아이디"></input>
+      <div className={styles.left}>
+        <h1>"Small habits create big changes. Let’s grow together!"</h1>
+        <img
+          src={logoImage}
+          alt="Study with us Logo"
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.LoginText}>Login</h1>
+        <input className={styles.IDbox} placeholder="아이디"></input>
+        <input className={styles.PSbox} placeholder="비밀번호"></input>
+        <div>
+          <button className={styles.ok}>확인</button>
+        </div>
+        <div>
+          <h2>Login with Others</h2>
+        </div>
+      </div>
     </div>
   );
 }
