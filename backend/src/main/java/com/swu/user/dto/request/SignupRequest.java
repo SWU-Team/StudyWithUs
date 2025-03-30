2b2f3324ed8ecbe235fec31fb975ab2866822b9e
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-03-30 19:54:15 +0900

    feat: SignupRequest DTO 구현

---
+++ backend/src/main/java/com/swu/user/dto/request/SignupRequest.java
@@ -0,0 +1,21 @@
+package com.swu.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String nickname;
+
+    private String profileImg;
+}
