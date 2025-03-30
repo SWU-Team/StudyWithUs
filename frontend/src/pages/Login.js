2066f008a7db277c5182bc7cdcb8f9925ab1cfc9
yuchangseuk <ckdtjr428@naver.com>
2025-03-30 23:53:37 +0900

    chore. 로그인 후 스터디룸 리스트로 이동#11
    -구글 및 카카오톡 프로토타입 공간 확보
    - 로그인 성공 후 페이지 이동 수정

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,30 @@
 import React, { useState } from "react";
 import styles from "../Styles/Login.module.css";
 import logoImage from "../assets/Study with us Logo.png";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [id, setId] = useState(""); // ID 상태
+  const [password, setPassword] = useState(""); // PW 상태
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 여부
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (id && password) {
+      alert("로그인 성공!");
+      navigate("/StudyRoomList"); // 로그인 성공 시 StudyRoomList로 이동
+    } else {
+      alert("아이디와 비밀번호를 입력해주세요");
+    }
+  };
+
+  if (isLoggedIn) {
+    return (
+      <div className={styles.div}>
+        <h1>어서오세요, {id}님!</h1>
+      </div>
+    );
+  }
   return (
     <div className={styles.div}>
       <div className={styles.left}>
@@ -15,13 +37,34 @@ function Login() {
       </div>
       <div className={styles.right}>
         <h1 className={styles.LoginText}>Login</h1>
-        <input className={styles.IDbox} placeholder="아이디"></input>
-        <input className={styles.PSbox} placeholder="비밀번호"></input>
+        <input
+          className={styles.IDbox}
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        ></input>
+        <input
+          className={styles.PSbox}
+          placeholder="비밀번호"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
         <div>
-          <button className={styles.ok}>확인</button>
+          <button className={styles.ok} onClick={handleLogin}>
+            확인
+          </button>
         </div>
         <div>
           <h2>Login with Others</h2>
+        </div>
+        <div>
+          <div className={styles.OAuth}>
+            <h4>구글 로그인</h4>
+          </div>
+          <div className={styles.OAuth}>
+            <h4>카카오톡로그인</h4>
+          </div>
         </div>
       </div>
     </div>
