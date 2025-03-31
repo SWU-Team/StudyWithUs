5b1040fa2c9c5a451a821945fe4556b0869ba40a
Hyoseok Seo <73714589+seohyoseok0408@users.noreply.github.com>
2025-03-31 15:24:58 +0900

    feat: Swagger 설정 (#14)
    * chore: Swagger 관련 의존성 추가
    
    * chore: Swagger 관련 경로 SecurityConfig에 허용
    
    * feat: 회원가입 API에 Swagger 문서 어노테이션 추가

--- backend/src/main/java/com/swu/user/controller/UserController.java
+++ backend/src/main/java/com/swu/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.swu.user.controller;
 import com.swu.global.response.ApiResponse;
 import com.swu.user.dto.request.SignupRequest;
 import com.swu.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "이메일, 비밀번호, 닉네임을 통해 회원가입을 수행합니다.")
     @PostMapping("/auth/signup")
     public ResponseEntity<ApiResponse<Void>> signup(@RequestBody @Valid SignupRequest request) {
         userService.signup(request);
