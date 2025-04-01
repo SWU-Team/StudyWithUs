c1e5523b50051ea2f005e0e84298934122585311
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-04-01 22:07:00 +0900

    feat: 로그인 요청 처리용 LoginFilter 구현

---
+++ backend/src/main/java/com/swu/auth/jwt/LoginFilter.java
@@ -0,0 +1,118 @@
+package com.swu.auth.jwt;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.swu.auth.domain.CustomUserDetails;
+import com.swu.auth.dto.request.LoginRequest;
+import com.swu.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StreamUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// JWT 사용한 인증 방식을 구현을 위해 커스텀 필터 작성.
+// UsernamePasswordAuthenticationFilter -> 로그인 가로챔
+@Slf4j
+public class LoginFilter extends UsernamePasswordAuthenticationFilter{
+
+    // 인증 요청 처리, 사용자 인증 정보 검증하는 역할.
+    // 커스텀 필터에서 사용하기 위해 주입받음
+    private final  AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    public LoginFilter (AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+
+        setFilterProcessesUrl("/auth/login");
+    }
+
+    // 로그인 요청을 처리하는 메소드
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException  {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ServletInputStream inputStream = request.getInputStream();
+            String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            LoginRequest loginRequest = objectMapper.readValue(messageBody, LoginRequest.class);
+
+            String username = loginRequest.getEmail();
+            String password = loginRequest.getPassword();
+
+            // 인증 토큰 생성
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+            // 인증 시도
+            return authenticationManager.authenticate(authToken);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //로그인 성공시 실행하는 메소드
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String username = customUserDetails.getUsername();
+        Long  id = customUserDetails.getUser().getId();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(username, role, id, 10 * 60 * 60 * 1000L); // 10시간 설정
+
+        // 헤더에 토큰 설정
+        response.setHeader("Authorization", "Bearer " + token);
+
+        // 바디 구성
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", id);
+        data.put("email", username);
+
+        ApiResponse<Map<String, Object>> apiResponse = ApiResponse.ok("로그인 성공", data);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            String responseBody = objectMapper.writeValueAsString(apiResponse);
+            response.getWriter().write(responseBody);
+        } catch (IOException e) {
+            log.error("JSON 직렬화 중 오류 발생", e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+
+        log.info("User {} login success", username);
+    }
+
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(401);
+
+        log.info("User login failed");
+    }
+}
