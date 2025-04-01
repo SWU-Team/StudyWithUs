793590339588ad3528021c4538ae13142620a819
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-04-02 00:18:41 +0900

    feat: WebSocket 의존성 및 ChatMessage DTO 설정

---
+++ backend/src/main/java/com/swu/chat/dto/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.swu.chat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType {
+        ENTER, // 입장
+        TALK,  // 채팅
+        EXIT   // 퇴장
+    }
+
+    private MessageType type;
+    private Long roomId;
+    private Long senderId;
+    private String senderNickname;
+    private String message;
+
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "type=" + type +
+                ", roomId=" + roomId +
+                ", senderId=" + senderId +
+                ", senderNickname='" + senderNickname + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
